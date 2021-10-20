33e933bdf8bfbc7e2b41cd7286bc3ecf12a65e85
zhangkun <kun.zhang@dtarray.com>
2021-10-20 17:55:36 +0800

    springboot-quartz

--- springboot-quartz/src/main/java/com/zk/quartz/config/QuartzConfig.java
+++ springboot-quartz/src/main/java/com/zk/quartz/config/QuartzConfig.java
@@ -1,30 +1,51 @@
 package com.zk.quartz.config;
 
-import com.zk.quartz.job.QuartzJob1;
+import com.zk.quartz.job.SimpleQuartzTask1;
+import com.zk.quartz.job.SimpleQuartzTask2;
 import org.quartz.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Quartz有3个核心要素：调度器(Scheduler)、任务(Job)、触发器(Trigger)
+ * Scheduler – 核心调度器
+ * Job – 任务
+ * JobDetail – 任务描述
+ * Trigger – 触发器（包括SimpleTrigger和CronTrigger）
  */
 @Configuration
 public class QuartzConfig {
 
     @Bean
-    public JobDetail jobDetail1(){
-        return JobBuilder.newJob(QuartzJob1.class).storeDurably().build();
+    public JobDetail simpleQuartzTaskDetail1(){
+        return JobBuilder.newJob(SimpleQuartzTask1.class).withIdentity("simpleQuartzTask1").storeDurably().build();
     }
 
     @Bean
-    public Trigger trigger1(){
-        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
-                .withIntervalInSeconds(1) //每一秒执行一次
-                .repeatForever(); //永久重复，一直执行下去
-        return TriggerBuilder.newTrigger()
-                .forJob(jobDetail1())
+    public Trigger simpleQuartzTaskTrigger1(){
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("*/5 * * * * ?");
+        return TriggerBuilder.newTrigger().forJob(simpleQuartzTaskDetail1())
+                .withIdentity("simpleQuartzTask1")
                 .withSchedule(scheduleBuilder)
                 .build();
     }
 
+
+    // 使用jobDetail包装job
+    @Bean
+    public JobDetail myJobDetail() {
+        return JobBuilder.newJob(SimpleQuartzTask2.class).withIdentity("simpleQuartzTask2").storeDurably().build();
+    }
+
+    // 把jobDetail注册到trigger上去
+    @Bean
+    public Trigger myJobTrigger() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(15).repeatForever();
+
+        return TriggerBuilder.newTrigger()
+                .forJob(myJobDetail())
+                .withIdentity("myJobTrigger")
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
 }
---
+++ springboot-quartz/src/main/java/com/zk/quartz/job/SimpleQuartzTask2.java
@@ -0,0 +1,30 @@
+package com.zk.quartz.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleQuartzTask2 extends QuartzJobBean {
+    /**
+     * 实现QuartzJobBean的executeInternal()方法，即可定义工作内容
+     */
+
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+        System.out.println("2-简单的单Quartz任务2开始---" + dateFormat.format(new Date()));
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            System.out.println("1-线程1阻塞失败。" + e);
+        }
+        System.out.println("2-简单的单Quartz任务2结束---" + dateFormat.format(new Date()));
+    }
+
+}
