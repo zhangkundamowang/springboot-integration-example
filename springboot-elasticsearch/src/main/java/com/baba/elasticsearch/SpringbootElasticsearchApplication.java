d324797233489213751e6610329e52f385c23a1e
zhangkun <kun.zhang@dtarray.com>
2021-12-21 10:50:53 +0800

    修改数据源

--- springboot-elasticsearch/src/main/java/com/baba/elasticsearch/SpringbootElasticsearchApplication.java
+++ springboot-elasticsearch/src/main/java/com/baba/elasticsearch/SpringbootElasticsearchApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootElasticsearchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootElasticsearchApplication.class, args);
+        SpringApplication.run(  SpringbootElasticsearchApplication.class, args);
     }
 
 }
--- springboot-kafka/src/main/java/com/baba/kafka/KafkaApplication.java
+++ springboot-kafka/src/main/java/com/baba/kafka/KafkaApplication.java
@@ -8,6 +8,10 @@ public class KafkaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaApplication.class, args);
+        //zookeeper版本 zookeeper-3.5.9    windows
+        //kafka版本 kafka_2.12-2.8.1       windows
+        //项目版本2.5.6
+        System.out.println("本地调试文档地址：http://localhost:8888/doc.html");
     }
 
 }
