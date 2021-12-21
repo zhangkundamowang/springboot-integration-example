3a310cddf49684ae67a138a98bcd0ea9f8143865
zhangkun <kun.zhang@dtarray.com>
2021-12-21 10:50:53 +0800

    springboot-kafka修改

--- springboot-kafka/src/main/java/com/baba/kafka/controller/KafkaConsumer.java
+++ springboot-kafka/src/main/java/com/baba/kafka/controller/KafkaConsumer.java
@@ -13,13 +13,13 @@ import java.util.Optional;
 @Slf4j
 @Component
 public class KafkaConsumer {
-    @KafkaListener(topics = KafkaProducer.TOPIC_TEST,groupId = KafkaProducer.TOPIC_GROUP1)
-    public void consumerGroup1(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic){
-        DateTime time= new DateTime();
+    @KafkaListener(topics = KafkaProducer.TOPIC_TEST, groupId = KafkaProducer.TOPIC_GROUP1)
+    public void consumerGroup1(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        DateTime time = new DateTime();
         String dateTime = time.toString("yyyy-MM-dd hh:mm:ss");
-        log.info("group1接收到消息时间：{}",dateTime);
+        log.info("group1接收到消息时间：{}", dateTime);
         Optional message = Optional.ofNullable(record.value());
-        if(message.isPresent()){
+        if (message.isPresent()) {
             Object msg = message.get();
             log.info("consumerGroup1 消费了： Topic:" + topic + ",Message:" + msg);
             //手动提交偏移量
@@ -27,13 +27,13 @@ public class KafkaConsumer {
         }
     }
 
-    @KafkaListener(topics = KafkaProducer.TOPIC_TEST,groupId = KafkaProducer.TOPIC_GROUP2)
-    public void consumerGroup2(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic){
-        DateTime time= new DateTime();
+    @KafkaListener(topics = KafkaProducer.TOPIC_TEST, groupId = KafkaProducer.TOPIC_GROUP2)
+    public void consumerGroup2(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        DateTime time = new DateTime();
         String dateTime = time.toString("yyyy-MM-dd hh:mm:ss");
-        log.info("group2接收到消息时间：{}",dateTime);
+        log.info("group2接收到消息时间：{}", dateTime);
         Optional message = Optional.ofNullable(record.value());
-        if(message.isPresent()){
+        if (message.isPresent()) {
             Object msg = message.get();
             log.info("consumerGroup2 消费了：Topic:" + topic + ",Message:" + msg);
             //手动提交偏移量
